4580d97b208f557e39487a71436a196d5ac358c2
Ayush W <113709655+AshWatts@users.noreply.github.com>
2024-05-10 22:53:54 +0530

    `

--- Assignment_5.cpp
+++ Assignment_5.cpp
@@ -1,153 +1,168 @@
-/* 	Implement all the functions of a dictionary (ADT) using open
-	hashing technique: separate chaining using linked list Data: Set of
-	(key, value) pairs, Keys are mapped to values, Keys must be
-	comparable, and Keys must be unique.
-	Standard Operations:
-	Insert (key, value), Find(key), Delete(key) 	*/
+/*
+Implement all the functions of a dictionary (ADT) using open
+hashing technique: separate chaining using linked list Data: Set of
+(key, value) pairs, Keys are mapped to values, Keys must be
+comparable, and Keys must be unique. Standard Operations: Insert
+(key, value), Find(key), Delete(key).
+*/
 
-#include <bits./stdc++.h>
+#include<iostream>
 using namespace std;
-
-class Node{
-public:
-	char value[20];
-	int key;
-	Node* next;
+ 
+class Node
+{
+    int key=0;
+    string value="";
+    Node *next=NULL;
+    friend class Dict;
 };
-
-class Open_Hashing{
-private:
-	int TableSize=10;
-	Node* HT[10];
-public:
-    Open_Hashing(){
-        for(int i=0; i<TableSize;i++)
+ 
+class Dict
+{
+    public:
+    Node *arr=new Node[10];
+ 
+    void insertValue(int key,string value)
+    {
+        int index=key%10;
+        if(arr[index].value=="")
         {
-            HT[i]=NULL;
-        }
-    }
-	//insert
-	void insertion(){
-        int number;
-        cout<<"Enter the number of records you want to enter: ";
-        cin>>number;
-        Node* Newnode;
-        int index;
-        Node* temp;
-        for(int i=0; i<number; i++){
-            Newnode = new Node;
-            cout<<"Enter the key: ";
-            cin>>Newnode->key;
-            cout<<"Enter the value: ";
-            cin>>Newnode->value;
-            Newnode->next=NULL;
-            index= (Newnode->key) % TableSize;
-
-            if(HT[index]==NULL){
-                HT[index]=Newnode;
-                //cout<<HT[index]->key<<" : "<<HT[index]->value<<endl;
+            arr[index].key=key;
+            arr[index].value=value;
+        } 
+        else
+        {
+            Node *New=new Node;
+            New->key=key;
+            New->value=value;
+            if(arr[index].next==NULL)
+            {
+                arr[index].next=New;
             }
-            else{
-                temp= HT[index];
-                while(temp->next!=NULL){
+            else
+            {
+                Node *temp=arr[index].next;
+                while(temp->next!=NULL)
+                {
                     temp=temp->next;
                 }
-                temp->next=Newnode;
+                temp->next=New;
             }
-
         }
-	}
-
-	//display
-	void display(){
-		Node* temp;
-		for(int index=0; index<TableSize; index++){
-			cout<<endl;
-			cout<<index<<" == ";
-			temp= HT[index];
-			while(temp!=NULL){
-				cout.width(4);
-				cout.setf(ios::left, ios::adjustfield);
-				cout<<temp->key<<" : ";
-				cout.width(25);
-				cout.setf(ios::left, ios::adjustfield);
-				cout<<temp->value<<" --> ";
+    }
+ 
+    void Display()
+    {
+        for(int i=0;i<10;i++)
+        {
+            cout<<"("<<arr[i].key<<","<<arr[i].value<<")";
+            Node *temp=arr[i].next;
+            while(temp!=NULL)
+            {
+                cout<<"->"<<"("<<temp->key<<","<<temp->value<<")";
                 temp=temp->next;
-			}
-			cout<<"NULL"<<endl;
-		}
-	}
-	//Find
-	void Find(){
-		int key;
-		cout<<"Enter the key you want to search: ";
-		cin>>key;
-		Node* curr;
-		bool check=false;
-		int index= key % TableSize;
-		curr=HT[index];
-		while(curr!=NULL){
-			if(curr->key== key){
-				cout<<"Key Found! Corresponding value is: "<<curr->value<<endl;
-				check =true;
-				break;
-			}
-			else{
-				curr=curr->next;
-			}
-		}
-		if(check==false){
-			cout<<"Key not found in the hash table..."<<endl;
-		}
-	}
-	//Delete
-	void deletion(){
-		int key;
-		cout<<"Enter the key you want to delete: ";
-		cin>>key;
-		int index=key%TableSize;
-		Node* curr;
-		Node*prev;
-		curr=HT[index];
-		bool check=false;
-		while(curr!=NULL){
-			if(curr->key ==key){
-				check=true;
-				break;
-			}
-			else{
-				prev=curr;
-				curr=curr->next;
-			}
-		}
-		if(check==true){
-			if(curr==HT[index]){
-				HT[index]=curr->next;
-				delete curr;
-			}
-			else{
-				prev->next=curr->next;
-				delete curr;
-			}
-		}
-		else
-			cout<<"Node to be deleted not found inside the hash table."<<endl;
-	}
+            }
+            cout<<endl;
+        }
+    }
+ 
+    void search(int key)
+    {
+        int index=key%10;
+        int compa=1;
+        if(arr[index].key==key)
+        {
+            cout<<"("<<arr[index].key<<","<<arr[index].value<<") is present."<<endl;
+            cout<<"Number of Comparisons Required : "<<compa<<endl;
+        }
+        else
+        {
+            Node *temp=arr[index].next;
+            while(temp!=NULL)
+            {
+                compa++;
+                if(temp->key==key)
+                {
+                    cout<<"("<<temp->key<<","<<temp->value<<") with comparisions: "<<compa<<endl;
+                    return;
+                }
+                temp=temp->next;
+            }
+            cout<<"No such value is present in dictionary."<<endl;
+            cout<<"Number of Comparisons Required : "<<compa<<"\n";
+        }
+ 
+    }
+ 
+    void Delete(int key)
+    {
+        int index=key%10;
+        if(arr[index].key==key)
+        {
+            if(arr[index].next==NULL)
+            {
+                arr[index].key=0;
+                arr[index].value="";
+            }
+            else
+            {
+                Node *temp=arr[index].next;
+                arr[index].key=temp->key;
+                arr[index].value=temp->value;
+                arr[index].next=temp->next;
+                temp->next=NULL;
+                delete temp;
+            }
+        }
+        else
+        {
+            Node *temp=arr[index].next;
+            if(temp==NULL)
+            {
+                cout<<"Key not Found!";
+                return;
+            }
+            if(temp->key==key)
+            {
+                arr[index].next=temp->next;
+                temp->next=NULL;
+                delete temp;
+            }
+            else
+            {
+                while(temp->next!=NULL)
+                {
+                    if(temp->next->key==key)
+                    {
+                        Node *temp1=temp;
+                        temp=temp->next;
+                        temp1->next=temp->next;
+                        delete temp;
+                        return;
+                    }
+                    temp=temp->next;
+                }
+                cout<<"Key not found";
+            }
+        }
+    }
 };
-int main() {
-	Open_Hashing H1;
-	H1.insertion();
-	H1.display();
-	H1.Find();
-	H1.Find();
-	H1.Find();
-	H1.Find();
-	H1.Find();
-	H1.deletion();
-	H1.deletion();
-	H1.deletion();
-	H1.deletion();
-	H1.deletion();
-	H1.display();
-	return 0;
+ 
+int main()
+{
+    Dict a;
+    a.insertValue(7,"Seven");
+    a.insertValue(27,"Twenty Seven");
+    a.insertValue(37,"Thirty Seven");
+    a.insertValue(15,"Fifteen");
+    a.insertValue(25,"Twenty Five");
+    a.insertValue(3,"Three");
+    a.Display();
+    cout<<"\n\n";
+    a.search(15);
+    cout<<"\n";
+    a.search(9);
+    a.Delete(7);
+    cout<<"\n\nDisplaying after Deletion : \n";
+    a.Display();
 }
---
+++ Assignment_6.cpp
@@ -0,0 +1,161 @@
+/*
+Represent a given graph using adjacency list to perform BFS and DFS. 
+Use the map of the area around the college as a graph.
+Identify the prominent land marks as nodes and perform DFS and BFS on that.
+*/
+ 
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+ 
+ 
+class Node
+{
+    string data="";
+    Node *down=NULL,*right=NULL;
+friend class Graph;
+};
+ 
+ 
+class Graph
+{
+    public:
+    int tot=0;
+    Node *head=new Node;
+ 
+    void Create()
+    {
+        cout<<"Enter number of vertices:";
+        ll a;cin>>tot;cout<<endl;a=tot;
+        Node *temp=head;
+        while(a--)
+        {
+            string str;ll b;
+            cout<<"Enter name of city and number of connected cities: ";
+            cin>>str>>b;cout<<endl;
+            Node *k=new Node;
+            k->data=str;
+            temp->down=k;
+            temp=k;Node *p=k;
+            while(b--)
+            {
+                cout<<"Enter connected city: ";string hk;cin>>hk;cout<<endl;
+                Node *New=new Node;
+                New->data=hk;
+                temp->right=New;
+                temp=New;
+            }
+            temp=p;
+        }
+    }
+    void Display()
+    {
+        Node *temp=head->down;
+        while(temp!=NULL)
+        {
+            Node *t2=temp->right;
+            cout<<temp->data;
+            while(t2!=NULL)
+            {
+                cout<<"->"<<t2->data;
+                t2=t2->right;
+            }
+            temp=temp->down;cout<<endl;
+        }
+ 
+    };
+    void Degree()
+    {
+        cout<<head->down->data<<endl;
+        map<string,ll>m;
+        Node *temp=head->down;
+        while(temp!=NULL){
+            Node *t2=temp->right;
+            ll count=0;
+            while(t2!=NULL)
+            {
+                count++;
+                t2=t2->right;
+            }
+            m[temp->data]=count;
+            temp=temp->down;
+        }
+        for(auto &it:m)
+        {
+            cout<<it.first<<" "<<it.second<<endl;
+        }
+ 
+    };
+ 
+    void DFS()
+    {
+        set<string>s;
+        stack<string>se;
+        string d = head->down->data;
+        se.push(head->down->data);
+        while(!se.empty())
+        {
+            string x=se.top();
+            se.pop();
+            if(s.find(x)==s.end())
+            {
+                cout<<x<<" ";
+                Node *temp=head->down;
+                s.insert(x);
+                while(temp->data!=x)
+                {
+                    temp=temp->down;
+                }
+                while(temp!=NULL)
+                {
+                    se.push(temp->data);
+                    temp=temp->right;
+                }
+            }
+        }
+    }
+ 
+    void BFS()
+    {
+        set<string>s;
+        queue<string>se;
+        string d = head->down->data;
+        se.push(head->down->data);
+        while(!se.empty())
+        {
+            string x=se.front();
+            se.pop();
+            if(s.find(x)==s.end())
+            {
+                cout<<x<<" ";
+                Node *temp=head->down;
+                s.insert(x);
+                while(temp->data!=x)
+                {
+                    temp=temp->down;
+                }
+                while(temp!=NULL)
+                {
+                    se.push(temp->data);
+                    temp=temp->right;
+                }
+            }
+        }
+    }
+};
+ 
+int main()
+{
+    Graph g;
+    g.Create();
+    cout<<"Adjacency matric of graph\n";
+    g.Display();
+    // cout<<"Degree of each vertex: \n";
+    // g.Degree();
+    cout<<"DFS : ";
+    g.DFS();
+    cout<<endl;
+    cout<<"BFS : ";
+    g.BFS();
+    cout<<endl;
+}
